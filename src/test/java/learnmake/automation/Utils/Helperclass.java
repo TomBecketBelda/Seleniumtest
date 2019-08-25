1f8ac2db5f8a903ca9cfaaff412fafd25e71a980
Tom Becket Belda <tomasbecket@gmail.com>
2019-08-25 19:15:44 +0200

    Test proves selenium

---
+++ src/test/java/learnmake/automation/Utils/Helperclass.java
@@ -0,0 +1,13 @@
+package learnmake.automation.Utils;
+
+public class Helperclass {
+
+   // private Helperclass (){
+
+   // }
+    public  void proba (String nome){
+
+        System.out.println("clase helper"+ nome);
+    }
+
+}
--- src/test/java/learnmake/automation/pages/NewPage.java
+++ src/test/java/learnmake/automation/pages/NewPage.java
@@ -1,11 +1,13 @@
 package learnmake.automation.pages;
 
+import learnmake.automation.Utils.Helperclass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class NewPage extends AbstractPage {
 
@@ -21,5 +23,9 @@ public class NewPage extends AbstractPage {
         searchField.clear();
         searchField.sendKeys("test tom selenium one");
         searchField.submit();
+
+        new Helperclass().proba("Tom");
     }
+
+
 }
